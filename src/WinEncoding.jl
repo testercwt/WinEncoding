22855af0fe43d366140de3f5c7de01e31f9ff077
Páll Haraldsson <Pall.Haraldsson@gmail.com>
2021-04-03 17:27:54 +0000

    Casing
    [skip ci]

--- src/WinEncoding.jl
+++ src/WinEncoding.jl
@@ -110,7 +110,7 @@ module WinEncoding
     """
         decode1252(a::Vector{UInt8})
     Convert an array of bytes a representing text in encoding cp1252 to a string.
-    [0x8d] => '\\u8d' as windows api does;no invalid sequence error;non-blocking
+    [0x8d] => '\\u8d' as Windows API does;no invalid sequence error;non-blocking
     """
     function decode1252(ss::Vector{UInt8}) 
         o=similar(ss, length(ss)*2+1)
